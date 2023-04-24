8ff2ba3df4e8a962c241a742ea133767bbf703f0
alxwnth <me@alxwnth.com>
2023-04-24 18:21:51 +0300

    feat: New design

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "./bootstrap.min"
+
+$('#collapseHead1').click(function () {
+    $("#open1").animate({
+        rotate: "90deg"
+    }, 500, function() {
+        $(this).css("-moz-transform", "rotate(90deg)");
+    });
+})
